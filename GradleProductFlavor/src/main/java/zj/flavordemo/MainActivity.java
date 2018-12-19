4bdfbace40c6a5470d8177bbc2c04d0135db4308
liuzhouliang <525688067@qq.com>
2018-12-19 10:15:41 +0800

    demo

---
+++ GradleProductFlavor/src/main/java/zj/flavordemo/MainActivity.java
@@ -0,0 +1,24 @@
+package zj.flavordemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+/**
+ * @ describe 
+ * 
+ * @author lzl
+ *
+ * @ time 2018/12/19 10:15
+ * 
+ * @ param
+ * 
+ * @ return
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
